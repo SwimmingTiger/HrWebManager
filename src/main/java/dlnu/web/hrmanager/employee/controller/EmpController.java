39a0b517d7805dd07c734d4c8025a7c2ee4f4978
黄英淏 <dell@pc>
2015-12-21 14:10:53 +0800

    实现显示和删除

---
+++ src/main/java/dlnu/web/hrmanager/employee/controller/EmpController.java
@@ -0,0 +1,114 @@
+package dlnu.web.hrmanager.employee.controller;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import dlnu.web.hrmanager.dept.dao.DeptDao;
+import dlnu.web.hrmanager.dept.entity.Dept;
+import dlnu.web.hrmanager.dept.service.DeptService;
+import dlnu.web.hrmanager.employee.dao.EmpDao;
+import dlnu.web.hrmanager.employee.entity.Emp;
+import dlnu.web.hrmanager.job.dao.JobDao;
+import dlnu.web.hrmanager.job.entity.Job;
+import dlnu.web.hrmanager.util.DBUtil;
+import dlnu.web.hrmanager.util.database.DBException;
+
+@Controller
+public class EmpController {
+	@RequestMapping(value = "/employee/*", method = RequestMethod.GET)
+	public String home(Locale locale, Model model) {
+		
+		try {
+				EmpDao dao = new EmpDao();
+				JobDao job = new JobDao();
+				ArrayList<Emp> emps = dao.Emplist();
+				ArrayList<Job> jobs = job.Display();
+				List<Dept> depts = DeptService.getInstance().getDeptList();
+		
+				model.addAttribute("empList", emps);
+				model.addAttribute("jobList", jobs);
+				model.addAttribute("deptList", depts);
+				
+		
+		} catch (DBException e) {
+			// TODO 自动生成的 catch 块
+			e.printStackTrace();
+		}
+		
+		return "employee/employee";
+	}
+	
+	@RequestMapping(value = "/employee/add", method = RequestMethod.POST)
+	public String add(Locale locale, Model model,
+			@RequestParam(value="empname") String empname,
+			@RequestParam(value="empsalary") Double empsalary,
+			@RequestParam(value="job") int emppost,
+			@RequestParam(value="empdate") String empdate,
+			@RequestParam(value="dept") int empdept) {
+		EmpDao dao = new EmpDao();
+		JobDao job = new JobDao();
+		
+		
+		
+		try {
+			Emp emp1 = new Emp();
+			emp1.setEmpID(new Integer(DBUtil.conn().getNextId("employee")));
+			emp1.setEmpname(empname);
+			emp1.setEmpsalary(empsalary);
+			emp1.setEmppost(job.Inquire(emppost));
+			emp1.setEmpdate(empdate);
+			emp1.setEmpdept(DeptService.getInstance().getDeptById(empdept));
+			
+			dao.Add(emp1);
+			
+		} catch (DBException e) {
+			// TODO 自动生成的 catch 块
+			e.printStackTrace();
+		}
+		
+		return home(locale, model);
+	}
+	
+	@RequestMapping(value = "/employee/delete", method = RequestMethod.POST)
+	public String delete(Locale locale, Model model,
+			@RequestParam(value="ID") int empID) {
+		JobDao job = new JobDao();
+		try {
+			Emp emp1 = new Emp();
+			emp1.setEmpID(empID);
+			
+			EmpDao dao3 = new EmpDao();
+			dao3.Dele(emp1);
+		} catch (DBException e) {
+			// TODO 自动生成的 catch 块
+			e.printStackTrace();
+		}
+		
+		
+		return home(locale, model);
+	}
+	
+	@RequestMapping(value = "/employee/edit", method = RequestMethod.POST)
+	public String edit(Locale locale, Model model,
+			@RequestParam(value="ID") int empID) {
+		EmpDao dao = new EmpDao();
+		
+		try {
+			Emp editEmp = dao.Inquire(empID);
+			model.addAttribute("editEmp", editEmp);
+		} catch (DBException e) {
+			// TODO 自动生成的 catch 块
+			e.printStackTrace();
+		}
+		
+		
+		return home(locale, model);
+	}
+}
---
+++ src/main/webapp/WEB-INF/views/employee/employee.jsp
@@ -0,0 +1,96 @@
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
+<%@ page session="false" pageEncoding="UTF-8" %>
+<html>
+<head>
+	<meta charset="UTF-8">
+	<title>Employee</title>
+</head>
+<body>
+
+<table>
+	<tr>
+		<th>ID</th>
+		<th>姓名</th>
+		<th>薪水</th>
+		<th>工作岗位</th>
+		<th>入职日期</th>
+		<th>所属部门</th>
+		<th> </th>
+	</tr>
+<!-- 使用 JSTL标签forEach循环显示员工  -->
+<c:forEach var="emp" items="${empList}">
+	<tr>
+		<td>${emp.empID}</td>
+		<td>${emp.empname}</td>
+		<td>${emp.empsalary}</td>
+		<td>${emp.emppost.name}</td>
+		<td>${emp.empdate}</td>
+		<td>${emp.empdept.name}</td>
+		<td>
+		<form action="delete" method="post">
+			<input type="hidden" name="ID" value="${emp.empID }">
+			<input type="submit" value="删除">
+		</form>
+		</td>
+		<td>
+		<form action="edit" method="post">
+		<input type="hidden" name="ID" value="${emp.empID }">
+		<input type="submit" value="编辑">
+		</form>
+		</td>
+	</tr>
+</c:forEach>
+	<tr>
+<c:choose>
+<c:when test="${empty editEmp}">
+	<form action="add" method="post">
+		<td></td>
+		<td><input name="empname"></td>
+		<td><input name="empsalary"></td>
+		<td>
+		<select name="job">
+			<c:forEach var="job" items="${jobList}">
+			<option value="${job.id}">${job.name}</option>
+			</c:forEach>
+		</select>
+		</td>
+		<td><input name="empdate"></td>
+		<td>
+		<select name="dept">
+			<c:forEach var="dept" items="${deptList}">
+			<option value="${dept.id}">${dept.name}</option>
+			</c:forEach>
+		</select>
+		</td>
+		<td><input type="submit" value="插入"></td>
+	</form>
+</c:when>
+<c:otherwise>
+	<form action="edit" method="post">
+		<td></td>
+		<td><input name="empname" value="${editEmp.empname} "></td>
+		<td><input name="empsalary" value="${editEmp.empsalary }"></td>
+		<td>
+		<select name="job">
+			<c:forEach var="job" items="${jobList}">
+			<option value="${job.id}">${job.name}</option>
+			</c:forEach>
+		</select>
+		</td>
+		<td><input name="empdate" value="${editEmp.empdate }"></td>
+		<td>
+		<select name="dept">
+			<c:forEach var="dept" items="${deptList}">
+			<option value="${dept.id}">${dept.name}</option>
+			</c:forEach>
+		</select>
+		</td>
+		<td><input type="submit" value="保存"></td>
+	</form>
+</c:otherwise>
+</c:choose>
+	</tr>
+</table>
+
+</body>
+</html>
